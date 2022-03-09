8f0a7c99c8b2315794d261b1ab9a93872e5203e6
302869442 <73424650+302869442@users.noreply.github.com>
2022-03-09 12:27:41 +0100

    Add files via upload

---
+++ templates/defaults/navbar.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul class="navbar">
+        <li class="nav-item"><a href="/" class="nav-link"></a></li>
+        <li class="nav-item"><a href="/contact" class="nav-link"></a></li>
+        <li class="nav-item"><a href="/login" class="nav-link"></a></li>
+    </ul>
+</nav>
